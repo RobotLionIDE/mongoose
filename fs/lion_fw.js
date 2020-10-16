a7f6747e5f5a4c07f140e6d698b4c0dfa4b4dad0
RobotLionIDE <kozlov.gv@robot-lion.ru>
2020-10-17 00:00:12 +0300

    V6

--- fs/lion_fw.js
+++ fs/lion_fw.js
@@ -2,24 +2,25 @@ let FWInit = function(){
   d.begin(0x2, 0x3C);
   d.clearDisplay();
   d.setTextColor(Adafruit_SH1106.WHITE);
-  d.setTextSize(1);
   d.setTextWrap(true);
 
+  d.setTextSize(2);
   d.setCursor(4,0);
   d.write(Cfg.get('bt.dev_name'));
-  d.setCursor(4,8);
-  d.write('Mongoose V: ' + Cfg.get('lion.firmware_version'));
+  d.setTextSize(1);
   d.setCursor(4,16);
+  d.write('Mongoose V: ' + Cfg.get('lion.firmware_version'));
+  d.setCursor(4,25);
   d.write('FW V: ' + Cfg.get('lion.fileware_version'));
   d.display();
 };
 
 let LionStart = function(){
-    return {status: '200'}
+    return {status: '200'};
 };
 
 let LionStop = function(){
-    return {status: '200'}
+    return {status: '200'};
 };
 
 let LionDo = function(d) {
