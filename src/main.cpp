6c506975871f05267e37a6e469b253655b870761
RobotLionIDE <kozlov.gv@robot-lion.ru>
2021-02-06 10:49:46 +0300

    SummRPC

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 
 #include "mgos.h"
 #include "mgos_sys_config.h"
+#include "mgos_rpc.h"
 
 #define SH1106_128_64
 #define OLED_SDA 21
@@ -28,8 +29,20 @@
 
 Adafruit_SH1106 display(SH1106_LCDWIDTH, SH1106_LCDHEIGHT);
 
+static void sum_cb(struct mg_rpc_request_info *ri, void *cb_arg, struct mg_rpc_frame_info *fi, struct mg_str args) {
+  double a = 0, b = 0;
+  if (json_scanf(args.p, args.len, ri->args_fmt, &a, &b) == 2) {
+    mg_rpc_send_responsef(ri, "%.2lf", a + b);
+  } else {
+    mg_rpc_send_errorf(ri, -1, "Bad request. Expected: {\"a\":N1,\"b\":N2}");
+  }
+  (void) cb_arg;
+  (void) fi;
+}
+
 enum mgos_app_init_result mgos_app_init(void) 
 {
+  mg_rpc_add_handler(mgos_rpc_get_global(), "Sum", "{a: %lf, b: %lf}", sum_cb, NULL);
   Wire.begin(SH1106_128_64);
   return MGOS_APP_INIT_SUCCESS;
 }
